2628e7d7d4bed8c29d2f1e34d8df05a9a1a958d3
SahutoL <greef.pandl@gmail.com>
2025-03-02 19:22:39 +0900

    change allowed hosts

--- devstore_be/settings.py
+++ devstore_be/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["devstore-9e9p.onrender.com"]
+ALLOWED_HOSTS = ["*", "devstore-9e9p.onrender.com"]
 
 
 # Application definition
